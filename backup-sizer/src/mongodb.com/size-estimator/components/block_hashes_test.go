e03ea3e24362cf80923f0fd420dae26c994a4e1e
Glenna Yu <glenna.yu@10gen.edu>
2015-07-07 11:29:59 -0400

    calculating params for bloomfilter

--- backup-sizer/src/mongodb.com/size-estimator/components/block_hashes_test.go
+++ backup-sizer/src/mongodb.com/size-estimator/components/block_hashes_test.go
@@ -2,17 +2,28 @@ package components
 
 import (
 	"testing"
-	"math"
+	"os"
+	"io/ioutil"
+	"path/filepath"
 	"fmt"
 )
 
+const emptyHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+const oneBlockHash = "bf718b6f653bebc184e1479f1935b8da974d701b893afcf49e701f3e2f9f9c5a"
+const fiveBlocksHash = "0e5b113b9f40bdd263fc20a75fc39dc112029e4b8c3b645e65856685465e7bd3"
+const subdirHash = "0e5b113b9f40bdd263fc20a75fc39dc112029e4b8c3b645e65856685465e7bd3"
+const partialBlockHash = "a1898b59a192f20e8f0a2a6fec39b171e6df0b19160c49023fca1f63808852b0"
+
+const emptyCompressed = 31
+const fiveBlocksCompressed = 369
+const oneBlockCompressed = 111
+const partialBlockCompressed = 372
+
+
 func TestWritingBlockHashes(test *testing.T) {
-//	session := dial(replset_port)
-//	session := dial(replset_wt_dirPerDb)
 	session := dial(standalone_mmap)
 
 	dbpath, err := GetDbPath(session)
-	dbpath = "/Users/gryu/test"
 	if err != nil {
 		test.Fatalf("Could not get dbpath. err:%v", err)
 	}
@@ -22,15 +33,210 @@ func TestWritingBlockHashes(test *testing.T) {
 		test.Errorf("failed. Err: %v", err)
 	}
 
-	fmt.Println("*******************************")
+	bs, err = GetBlockHashes(dbpath, "hashes", 1)
+	if err != nil {
+		test.Errorf("Failed on iteration2. Err:%v", err)
+	}
+
+	if bs.DedupRate != 1 {
+		test.Errorf("deduprate < 1 on exactly same data. Received: %f", bs.DedupRate)
+	}
+}
+
+func TestErrorHandling(test *testing.T) {
+	// test error handling
+	rootonlydir := "hashes_root"
+
+	bs, err := GetBlockHashes(rootonlydir, rootonlydir, 3)
+	if bs != nil {
+		test.Errorf("Expected nil block, received %v", bs)
+	}
+	if err == nil {
+		test.Errorf("Expected errors, received none")
+	}
+	fmt.Println(err)
+}
+
+func TestDirPerDB(test *testing.T) {
+	session := dial(replset_wt_dirPerDb)
+
+	dbpath, err := GetDbPath(session)
+	if err != nil {
+		test.Fatalf("Could not get dbpath. err:%v", err)
+	}
+
+	bs, err := GetBlockHashes(dbpath, "hashes", 0)
+	if err != nil {
+		test.Errorf("failed. Err: %v", err)
+	}
 
 	bs, err = GetBlockHashes(dbpath, "hashes", 1)
 	if err != nil {
 		test.Errorf("Failed on iteration2. Err:%v", err)
 	}
-	fmt.Println("*******************************")
 
-	if bs.DedupRate != 1 && bs.DedupRate != math.NaN() {
+	if bs.DedupRate != 1 {
 		test.Errorf("deduprate < 1 on exactly same data. Received: %f", bs.DedupRate)
+	}
+}
+
+func TestReadFileNames(test *testing.T) {
+	// empty directory
+	errCh := make(chan error)
+
+	exists, err := checkExists(empty_dir)
+	if err != nil {
+		test.Fatalf("Failure to check %s exists", empty_dir)
+	}
+	if !exists {
+		test.Errorf("%s does not exist", empty_dir)
+	}
+
+	var fnCh chan string
+	go func() {
+		fnCh = readFileNamesToChannel("./DoesNotExist", errCh)
+	}()
+	err = <-errCh
+	if err == nil {
+		test.Errorf("Expected an error")
+	}
+	fn, open := <-fnCh
+	if open {
+		test.Errorf("Failed to close filename channel. File:%s", fn)
+	}
+	if fn != "" {
+		test.Errorf("Unexpected filename in non-existant directory:%s", fn)
+	}
+
+	go func() {
+		fnCh = readFileNamesToChannel(empty_dir, errCh)
+	}()
+	fn, open = <-fnCh
+	if open {
+		test.Errorf("Failed to close filename channel. File:%s", fn)
+	}
+	if fn != "" {
+		test.Errorf("Unexpected filename in empty directory:%s", fn)
+	}
+
+	fnCh = readFileNamesToChannel(TestDataDir, errCh)
+	fncount := 0
+	for fn := range fnCh {
+		fi, err := os.Stat(fn)
+		if err != nil {
+			test.Errorf("Error with file %s. Error: &v", fn, err)
+		} else if fi.IsDir() {
+			test.Errorf("Unexpected directory in filename channel: %s", fn)
+		} else {
+			fncount++
+		}
+	}
+	if fncount != 5 {
+		test.Errorf("Expected five filenames from directory %s. Received: %d", TestDataDir, fncount)
+	}
+
+}
+
+func TestSplitBlocks(test *testing.T) {
+	numBlocks := map[string]int {
+		"empty.test" : 0,
+		"oneblock.test" : 1,
+		"fiveblocks.test" : 5,
+		"subdir.test" : 5,
+		"partialblock.test": 6,
+	}
+	fns, err := getFilesInDir(TestDataDir, false)
+	if err != nil {
+		test.Fatalf(err.Error())
+	}
+	for _, fn := range fns {
+		blocks, err := splitFiles(fn)
+		if err != nil {
+			test.Errorf("Failed to split file %s into blocks. Error: %v", fn, err)
+		}
+		fn = filepath.Base(fn)
+		if len(blocks) != numBlocks[fn] {
+			test.Errorf("Unexpected number of blocks for file %s. Expected: %d, Received:%d", fn, numBlocks[fn],
+				len(blocks))
+		}
+	}
+
+}
+
+func TestHashAndCompressBlocks(test *testing.T) {
+	hashes := map[string]string{
+		"empty.test" : emptyHash,
+		"oneblock.test" : oneBlockHash,
+		"fiveblocks.test" : fiveBlocksHash,
+		"subdir.test" : fiveBlocksHash,
+		"partialblock.test": partialBlockHash,
+	}
+
+//	compressedSizes := map[string]int{
+//		"empty.test" : emptyCompressed,
+//		"oneblock.test" : oneBlockCompressed,
+//		"fiveblocks.test" : fiveBlocksCompressed,
+//		"partialblock.test": partialBlockCompressed,
+//	}
+
+	fns, err := getFilesInDir(TestDataDir, false)
+	if err != nil {
+		test.Fatalf(err.Error())
+	}
+	for _, fn := range fns {
+		f, err := os.Open(fn)
+		fn := filepath.Base(fn)
+
+		var b []byte
+		b, err = ioutil.ReadAll(f)
+		if err != nil {
+			test.Fatalf(err.Error())
+		}
+		block, err := hashAndCompressBlocks(b)
+		if err != nil {
+			test.Errorf(err.Error())
+		}
+
+		h := block.hash
+		if h != hashes[fn] {
+			test.Errorf("Incorrect hash for file %s. Expected:%s Received:%s", fn,  hashes[fn], h)
+		}
+
+//		compressedSize := block.compressedSize
+//		if compressedSize != compressedSizes[fn] {
+//			test.Errorf("Incorrect compressed sizes for file %s. Expected:%d Received:%d",fn, compressedSizes[fn],
+//				compressedSize)
+//		}
+
+	}
+}
+
+func TestLoadPrevHashes(test *testing.T) {
+	// file doesn't exist
+	const m, k = 10, 2
+	fn := "./DoesNotExist"
+	_, err := loadPrevHashes(fn, m, k)
+	if err != nil {
+		test.Errorf("Unexpected error from loading non-existant file %s. Error: %v", fn, err)
+	}
+
+	// empty file -- empty bloom filter
+	fn = TestDataDir + "/empy.test"
+	_, err = loadPrevHashes(fn, m, k)
+	if err != nil {
+		test.Errorf("Unexpected error from loading empty file %s. Error: %v", fn, err)
+	}
+
+	// reading from directory should return an error
+	fn = TestDataDir
+	_, err = loadPrevHashes(fn, m, k)
+	if err == nil {
+		test.Errorf("Expected error from loading directory %s", fn)
+	}
+
+	fn = TestDataDir + "/empy.test"
+	_, err = loadPrevHashes(fn, m, k)
+	if err != nil {
+		test.Errorf("Unexpected error from loading file %s. Error: %v", fn, err)
 	}
 }
